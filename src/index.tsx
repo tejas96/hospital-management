aec88c472856a7b4da00849306ad1b60941dcc8c
tejas96 <tejas96patil@gmail.com>
2022-02-04 22:21:08 +0530

    setup boiler plate and firebase

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./config/firebase";
+
+ReactDOM.render(
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById("root")
+);
