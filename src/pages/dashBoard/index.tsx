ecfc3b219b43ab520696e832467373be2f2e0c94
tejas96 <tejas96patil@gmail.com>
2022-03-06 14:42:13 +0530

    add sideNav config and routes name

---
+++ src/pages/dashBoard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderAndDrawer } from "src/common/components";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <HeaderAndDrawer />
+    </div>
+  );
+};
+
+export default Dashboard;
