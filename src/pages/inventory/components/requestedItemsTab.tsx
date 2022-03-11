351800e8bb0c44aafc724a13ee993ba31d758fec
tejas96 <tejas96patil@gmail.com>
2022-03-11 21:37:46 +0530

    inventory page implimentetion

---
+++ src/pages/inventory/components/requestedItemsTab.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@material-ui/core";
+import React from "react";
+interface IProps {}
+const RequestedItems: React.FC<IProps> = () => {
+  return <Box>RequestedItems</Box>;
+};
+
+export default RequestedItems;
--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -1,13 +1,31 @@
-import React from "react";
-import { Box } from "@material-ui/core";
-import { HeaderAndDrawer } from "src/common/components";
+import React, { useState } from "react";
+import { Box, Divider } from "@material-ui/core";
+import { Button, HeaderAndDrawer, Modal, Text } from "src/common/components";
+import BasicTabs from "src/pages/inventory/components/tabWindow";
 interface IProps {}
 
 const Inventory: React.FC<IProps> = () => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <>
       <HeaderAndDrawer />
-      <Box></Box>
+      <Box className="w-full flex justify-between items-center p-5">
+        <Text variant="h4">Inventory</Text>
+        <Button
+          onClick={() => setOpen(true)}
+          className="bg-primary"
+          label="Create RFP"
+        />
+      </Box>
+      <Divider />
+      <Box>
+        <BasicTabs></BasicTabs>
+        <Modal onClose={() => setOpen(false)} open={open}>
+          <Box className="flex flex-col">
+            <Box className="w-96 h-96 bg-slate-300"></Box>
+          </Box>
+        </Modal>
+      </Box>
     </>
   );
 };
