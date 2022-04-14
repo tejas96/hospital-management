0c354dd0e0cb3cadc91f332648b7e57755767a6d
tejas96 <tejas96patil@gmail.com>
2022-04-14 08:28:20 +0530

    update chat icon

--- src/pages/chatBot/index.tsx
+++ src/pages/chatBot/index.tsx
@@ -9,11 +9,19 @@ const ChatBot: React.FC<{}> = () => {
   return (
     <Box className="fixed bottom-0 right-0 m-16 flex justify-end flex-col items-end z-10 ">
       {toggleChat && <ChatBox />}
-      <ChatBubble
-        onClick={() => setToggleChat((prev) => !prev)}
-        fontSize="large"
-        className="text-primary cursor-pointer"
-      />
+      <Box className="relative">
+        {!toggleChat && (
+          <span className="absolute right-0 -top-1 w-5 h-5 bg-red-600 rounded-full text-center leading-3 text-white">
+            1
+          </span>
+        )}
+        <ChatBubble
+          onClick={() => setToggleChat((prev) => !prev)}
+          fontSize="large"
+          style={{ fontSize: "80px" }}
+          className="text-primary cursor-pointer"
+        />
+      </Box>
     </Box>
   );
 };
